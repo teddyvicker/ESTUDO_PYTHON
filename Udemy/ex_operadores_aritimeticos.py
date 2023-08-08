dcce664676dbd5f22ac89f505524412b003d8afa
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-08-08 12:17:28 -0400

    operadores aritimeticos

---
+++ Udemy/ex_operadores_aritimeticos.py
@@ -0,0 +1,43 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
+
+concatenacao = 'vic' + ' ' + 'oliveira' #união
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10     #repetição
+tres_vezes_vic = 3 * 'vic'
+print(a_dez_vezes)
+print(tres_vezes_vic)
+
+# precedencia de aritimeticos 
+
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
