3b9400f6a5057bd78365d561df12817b960768d7
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-08-22 19:29:36 -0400

    exercicio do livro pg.49 - 2.5 - introdução a programção em python

---
+++ intro_pyhton_book/pg_49_03.py
@@ -0,0 +1,9 @@
+#Escreva um programa que calcule 3 números e exiba o a da soma dos tres na tela
+
+num_01 = int(input())
+num_02 = int(input())
+num_03 = int(input())
+
+resulta_soma = (num_01 + num_02 + num_03)
+
+print("O valor da soma dos tres números é:", resulta_soma)5
