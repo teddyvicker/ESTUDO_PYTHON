e6d5e2b4c1c24e853610fc8ee5f3acd4b985cec6
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-08-29 22:18:52 -0400

    exercicio de if udemy

---
+++ Udemy/ex_if.py
@@ -0,0 +1,8 @@
+entrada = input('Voce quer entrar ou sair do sistema?')
+
+if entrada == 'entrar':
+    print('Voce entrou no sistema')
+elif entrada == 'sair':
+    print('voce saiu do sistema')
+else:
+    print('error: voce precisa digitar entrar ou sair')
