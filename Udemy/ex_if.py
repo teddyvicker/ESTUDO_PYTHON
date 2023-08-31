858c3464e5fa2de6bdef22045b7d2159d7a09381
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-08-31 16:02:27 -0400

    exemplo de analise de dados if e match

--- Udemy/ex_if.py
+++ Udemy/ex_if.py
@@ -1,4 +1,5 @@
 entrada = input('Voce quer entrar ou sair do sistema?')
+#entrada = 2 ou 1
 
 if entrada == 'entrar':
     print('Voce entrou no sistema')
@@ -6,3 +7,13 @@ elif entrada == 'sair':
     print('voce saiu do sistema')
 else:
     print('error: voce precisa digitar entrar ou sair')
+
+#match entrada:
+#    case 1:
+#        print('Voce entrou no sistema')
+#    case 2:
+#        print('voce saiu do sistema')
+#    case _ :
+#        print('error: voce precisa digitar entrar ou sair')
+
+#tipos de analise de dados com if e match
