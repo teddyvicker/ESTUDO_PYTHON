d703f08c1cdd629eb20249095e98bcf6aa0ca288
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-10-06 00:00:37 -0400

    exercicio if e comparação

---
+++ Udemy/ex_if_comparacao.py
@@ -0,0 +1,7 @@
+valor_1 = input('digite um valor:')
+valor_2 = input('digite outro valor:')
+
+if valor_1 > valor_2 :
+    print ('maior valor é:', valor_1)
+else:
+    print ('maior valor é:', valor_2)
