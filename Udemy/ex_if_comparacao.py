404085b65dd3893fd55b7aae92aadc06e5c28bb5
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-10-16 23:26:18 -0400

    atualizaçao do ex

--- Udemy/ex_if_comparacao.py
+++ Udemy/ex_if_comparacao.py
@@ -2,6 +2,14 @@ valor_1 = input('digite um valor:')
 valor_2 = input('digite outro valor:')
 
 if valor_1 > valor_2 :
-    print ('maior valor é:', valor_1)
+    print (
+    f'{valor_1} é maior do que {valor_2}' 
+    )
+elif valor_1 == valor_2:
+    print (
+    f'{valor_1} e o {valor_2} são iguais'
+    )
 else:
-    print ('maior valor é:', valor_2)
+    print (
+    f'{valor_2} é maior do que {valor_1}'
+)
