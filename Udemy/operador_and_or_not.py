4badfb92254df891cf2f3902785f4cd68487af60
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2023-10-26 16:50:09 -0400

    exemplo da aula de and

--- Udemy/operador_and_or_not.py
+++ Udemy/operador_and_or_not.py
@@ -8,16 +8,17 @@
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
-# entrada = input('[E]ntrar [S]air: ')
-# senha_digitada = input('Senha: ')
 
-# senha_permitida = '123456'
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
 
-# if entrada == 'E' and senha_digitada == senha_permitida:
-#     print('Entrar')
-# else:
-#     print('Sair')
+senha_permitida = '123456'
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+        print('Entrar')
+else:
+        print('Sair')
 
 # Avaliação de curto circuito
-print(True and False and True)
-print(True and 0 and True)
+#print(True and False and True)
+#print(True and 0 and True)
