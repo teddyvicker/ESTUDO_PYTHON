09189af37464b97669763ae5b4d6863752f116d9
Caroline Victoria Oliveira <vicpoliveira@gmail.com>
2024-04-10 17:37:53 -0400

    é isso

--- Rocketseat/Módulo_01/modulos.py
+++ Rocketseat/Módulo_01/modulos.py
@@ -6,7 +6,8 @@ print(f"A Raiz quandrada de 25 é: {raiz_quadrada}")
 
 print("\n Exemplo de criação de utilização de um modulo>")
 from meu_modulo import saudacao, dobro 
-mensagem = meu_modulo.saudacao("Vic")
-resultado_dobro = meu_modulo.dobro(5)
+
+mensagem = saudacao("Vic")
+resultado_dobro = dobro(5)
 print(mensagem)
 print(resultado_dobro)
